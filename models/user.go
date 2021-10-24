292226c5bac9ba31b03ce564701755d0d68c1636
nade-harlow <omonadefranklyn@gmail.com>
2021-10-24 14:37:41 +0100

    added gorm type

--- models/user.go
+++ models/user.go
@@ -7,13 +7,13 @@ import (
 // User holds a user details
 type User struct {
 	ID              string `gorm:"type:uuid;primaryKey;default:uuid_generate_v4()"`
-	FirstName       string `json:"first_name" binding:"required" form:"first_name"`
-	LastName        string `json:"last_name" binding:"required" form:"last_name"`
-	Phone1          string `json:"phone" binding:"required" form:"phone1"`
+	FirstName       string `json:"first_name" gorm:"type:not null" binding:"required" form:"first_name"`
+	LastName        string `json:"last_name" gorm:"type:not null" binding:"required" form:"last_name"`
+	Phone1          string `json:"phone" gorm:"type:not null" binding:"required" form:"phone1"`
 	Phone2          string `json:"phone_2" form:"phone2"`
-	Email           string `json:"email" binding:"required,email" form:"email"`
-	Address         string `json:"address" binding:"required" form:"address"`
-	Password        string `json:"-" binding:"required" form:"password"`
+	Email           string `json:"email" gorm:"type:not null" binding:"required,email" form:"email"`
+	Address         string `json:"address" gorm:"type:not null" binding:"required" form:"address"`
+	Password        string `json:"-" gorm:"type:not null" binding:"required" form:"password"`
 	ConfirmPassword string `json:"confirm_password" form:"confirm_password"`
 	Image           string `json:"image,omitempty"`
 	//RoleID          Role      `json:"role_id" gorm:"foreignKey:RoleID"`

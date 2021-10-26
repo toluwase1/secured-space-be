5dccead09f65af364946c39ad4e47a12b5a76e2c
techagentng <techagentng>
2021-10-26 09:10:27 +0100

    struct in progress

--- models/category.go
+++ models/category.go
@@ -2,5 +2,8 @@ package models
 
 type Category struct {
 	ID        string    `json:"id"`
-	CategoryName string `json:"category_name"`
+	Shop string `json:"shop"`
+	OfficeSpace string `json:"office-space"`
+	FlatsApartments string `json:"category_name"`
+	Name string `json:"category_name"`
 }

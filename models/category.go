2224dd9b83fe2e06cf5591f2a5cedba162e6bf04
techagentng <techagentng>
2021-10-26 09:50:52 +0100

    model created

--- models/category.go
+++ models/category.go
@@ -1,9 +1,10 @@
 package models
 
+import "time"
+
 type Category struct {
-	ID        string    `json:"id"`
-	Shop string `json:"shop"`
-	OfficeSpace string `json:"office-space"`
-	FlatsApartments string `json:"category_name"`
-	Name string `json:"category_name"`
+	ID        string `json:"id"`
+	Name      string `json:"shop"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }

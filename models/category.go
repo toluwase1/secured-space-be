ddd9897d7f038b62063721e669b4c3e727f6de24
techagentng <techagentng>
2021-10-26 10:21:19 +0100

    model created

--- models/category.go
+++ models/category.go
@@ -4,7 +4,7 @@ import "time"
 
 type Category struct {
 	ID        string `json:"id"`
-	Name      string `json:"Name"`
+	Name      string `json:"name"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }

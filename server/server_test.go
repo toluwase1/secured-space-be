6ab78399b6e7872e99d5a3c995ba6970003d4416
tambarie <gt.gbaragbo@gmail.com>
2021-10-27 09:07:00 +0100

    added test for if email exists

--- server/server_test.go
+++ server/server_test.go
@@ -15,39 +15,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSignupWithCorrectDetails(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	m := db.NewMockDB(ctrl)
-
-	s := &Server{
-		DB:     m,
-		Router: router.NewRouter(),
-	}
-	router := s.setupRouter()
-
-	user := models.User{
-		FirstName: "Spankie",
-		LastName:  "Dee",
-		Password:  "password",
-		Address:   "1, boli drive",
-		Email:     "spankie_signup@gmail.com",
-		Phone1:    "08909876787",
-	}
-
-	m.EXPECT().CreateUser(gomock.Any()).Return(&user, nil)
-
-	jsonuser, err := json.Marshal(user)
-	if err != nil {
-		t.Fail()
-		return
-	}
-	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("POST", "/api/v1/auth/signup", strings.NewReader(string(jsonuser)))
-	router.ServeHTTP(w, req)
-
-	assert.Equal(t, http.StatusCreated, w.Code)
-	assert.Contains(t, w.Body.String(), "signup successful")
-}
 
 func TestSignupWithInCorrectDetails(t *testing.T) {
 	ctrl := gomock.NewController(t)
@@ -73,10 +40,80 @@ func TestSignupWithInCorrectDetails(t *testing.T) {
 		return
 	}
 	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("POST", "/api/v1/auth/signup", strings.NewReader(string(jsonuser)))
+	req, _ := http.NewRequest("POST", "/api/v1/auth/signup_tenant", strings.NewReader(string(jsonuser)))
 	router.ServeHTTP(w, req)
 
 	bodyString := w.Body.String()
 	assert.Equal(t, http.StatusBadRequest, w.Code)
 	assert.Contains(t, bodyString, fmt.Sprintf("validation failed on field 'Email', condition: email, actual: %s", user.Email))
 }
+
+func TestSignupIfEmailExists(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	m := db.NewMockDB(ctrl)
+
+	s := &Server{
+		DB:     m,
+		Router: router.NewRouter(),
+	}
+	router := s.setupRouter()
+
+	user := models.User{
+		FirstName: "Spankie",
+		LastName:  "Dee",
+		Password:  "password",
+		Address:   "1, boli drive",
+		Email:     "spankie_signup@gmail.com",
+		Phone1:    "08909876787",
+	}
+
+	m.EXPECT().FindUserByEmail(gomock.Any()).Return(&user, nil)
+
+	jsonuser, err := json.Marshal(user)
+	if err != nil {
+		t.Fail()
+		return
+	}
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/api/v1/auth/signup_tenant", strings.NewReader(string(jsonuser)))
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusNotFound, w.Code)
+	assert.Contains(t, w.Body.String(), "User email already exists")
+}
+
+func TestSignupWithCorrectDetails(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	m := db.NewMockDB(ctrl)
+
+	s := &Server{
+		DB:     m,
+		Router: router.NewRouter(),
+	}
+	router := s.setupRouter()
+
+	user := models.User{
+		FirstName: "Spankie",
+		LastName:  "Dee",
+		Password:  "password",
+		Address:   "1, boli drive",
+		Email:     "spankie_signup@gmail.com",
+		Phone1:    "08909876787",
+	}
+
+	m.EXPECT().CreateUser(user.Email).Return(&user, nil)
+
+	jsonuser, err := json.Marshal(user)
+	if err != nil {
+		t.Fail()
+		return
+	}
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/api/v1/auth/signup_tenant", strings.NewReader(string(jsonuser)))
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusCreated, w.Code)
+	assert.Contains(t, w.Body.String(), "signup successful")
+}
+
+

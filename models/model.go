58d2ec604856642b9401efa1d7d4cca0be34e9ed
tambarie <gt.gbaragbo@gmail.com>
2021-10-27 09:03:22 +0100

    removed autogenerated uuid from the user models struct

--- models/model.go
+++ models/model.go
@@ -3,7 +3,7 @@ package models
 import "time"
 
 type Models struct {
-	ID        string    `gorm:"type:uuid;primaryKey;default:uuid_generate_v4()"`
+	ID        string    `gorm:"type:uuid;primaryKey;"`
 	CreatedAt time.Time `json:"created_at,omitempty"`
 	UpdatedAt time.Time `json:"updated_at,omitempty"`
 }

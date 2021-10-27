f8a0e15b338845b6d4f7a93f339e312d4608d1c8
tambarie <gt.gbaragbo@gmail.com>
2021-10-27 09:00:29 +0100

    added two methods find_user_by_email and find user by username

--- db/postgres.go
+++ db/postgres.go
@@ -18,33 +18,47 @@ type PostgresDB struct {
 func (postgresDB *PostgresDB) Init() {
 	// Database Variables
 	DBUser := os.Getenv("DB_USER")
-	//DBPass := os.Getenv("DB_PASS")
+	DBPass := os.Getenv("DB_PASS")
 	DBHost := os.Getenv("DB_HOST")
 	DBName := os.Getenv("DB_NAME")
 	DBPort := os.Getenv("DB_PORT")
 	DBTimeZone := os.Getenv("DB_TIMEZONE")
 	DBMode := os.Getenv("DB_MODE")
 
-	dsn := fmt.Sprintf("host=%v user=%v dbname=%v port=%v sslmode=%v TimeZone=%v",DBHost,DBUser,DBName,DBPort,DBMode,DBTimeZone)
+	dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v sslmode=%v TimeZone=%v", DBHost, DBUser, DBPass,DBName, DBPort, DBMode, DBTimeZone)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("failed to connect database: %v", err)
 	}
 	postgresDB.DB = db
+	//err = postgresDB.DB.AutoMigrate(&models.Role{})
+	//roles := []models.Role{{Title: "tenant"},{Title: "agent"}}
+	//postgresDB.DB.Create(roles)
+	//if err != nil {
+	//	return
+	//}
+	err = postgresDB.DB.AutoMigrate(&models.User{})
+	if err != nil {
+		fmt.Printf("err %v:",err.Error())
+
+	}
+
 }
 
-
-
+var user *models.User
 
 func (postgresDB *PostgresDB) CreateUser(user *models.User) (*models.User, error) {
 	result := postgresDB.DB.Create(&user)
 	return user, result.Error
 }
 func (postgresDB *PostgresDB) FindUserByUsername(username string) (*models.User, error) {
-	return nil, nil
+	result := postgresDB.DB.Where("username = ?",username).First(&user)
+	return user, result.Error
+
 }
 func (postgresDB *PostgresDB) FindUserByEmail(email string) (*models.User, error) {
-	return nil, nil
+	result := postgresDB.DB.Where("email = ?",email).First(&user)
+	return user, result.Error
 }
 func (postgresDB *PostgresDB) UpdateUser(user *models.User) error {
 	return nil

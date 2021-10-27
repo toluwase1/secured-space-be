7e571754a18ee2f2b276ecb0e276d35ee7bfd6fd
Ad3bay0c <aclintonadebayo@gmail.com>
2021-10-27 20:56:45 +0100

    SaveBookmarkApartment handler added

--- server/bookmark_handler.go
+++ server/bookmark_handler.go
@@ -1,1 +1,20 @@
 package server
+
+import (
+	"github.com/decadevs/rentals-api/models"
+	"github.com/decadevs/rentals-api/server/response"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func (s *Server) SaveBookmarkApartment() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		bookmarkApartment := &models.BookmarkApartment{}
+
+		if errs := s.decode(c, bookmarkApartment); errs != nil {
+			response.JSON(c, "", http.StatusBadRequest, nil, errs)
+			return
+		}
+
+	}
+}

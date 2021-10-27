e175538e5991d809cff55309274a1c572bed53f6
tambarie <gt.gbaragbo@gmail.com>
2021-10-27 23:37:47 +0100

    finduserbyemail method fixed

--- db/postgres.go
+++ db/postgres.go
@@ -25,42 +25,31 @@ func (postgresDB *PostgresDB) Init() {
 	DBTimeZone := os.Getenv("DB_TIMEZONE")
 	DBMode := os.Getenv("DB_MODE")
 
-
-
 	dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v sslmode=%v TimeZone=%v", DBHost, DBUser, DBPass, DBName, DBPort, DBMode, DBTimeZone)
-
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("failed to connect database: %v", err)
 	}
 	postgresDB.DB = db
-	//err = postgresDB.DB.AutoMigrate(&models.Role{})
-	//roles := []models.Role{{Title: "tenant"},{Title: "agent"}}
-	//postgresDB.DB.Create(roles)
-	//if err != nil {
-	//	return
-	//}
 	err = postgresDB.DB.AutoMigrate(&models.User{})
 	if err != nil {
 		fmt.Printf("err %v:", err.Error())
-
 	}
-
 }
 
-var user *models.User
-
 func (postgresDB *PostgresDB) CreateUser(user *models.User) (*models.User, error) {
-	result := postgresDB.DB.Create(&user)
+	result := postgresDB.DB.Create(user)
 	return user, result.Error
 }
 func (postgresDB *PostgresDB) FindUserByUsername(username string) (*models.User, error) {
+	var user *models.User
 	result := postgresDB.DB.Where("username = ?", username).First(&user)
 	return user, result.Error
-
 }
+
 func (postgresDB *PostgresDB) FindUserByEmail(email string) (*models.User, error) {
+	var user *models.User
 	result := postgresDB.DB.Where("email = ?", email).First(&user)
 	return user, result.Error
 }

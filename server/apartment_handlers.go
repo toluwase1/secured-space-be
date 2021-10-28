4f278227af437f8501f729e0c6c3c17a0a51bd08
Ad3bay0c <aclintonadebayo@gmail.com>
2021-10-28 23:13:47 +0100

    Delete Apartment Handler Logics added

--- server/apartment_handlers.go
+++ server/apartment_handlers.go
@@ -1,9 +1,34 @@
 package server
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/decadevs/rentals-api/models"
+	"github.com/decadevs/rentals-api/server/response"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
 
 func (s *Server) DeleteApartment() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
+		if userI, exists := c.Get("user"); exists {
+			if user, ok := userI.(*models.User); ok {
+				apartmentID := c.Param("apartmentID")
+				if apartmentID == "" {
+					response.JSON(c, "", http.StatusBadRequest, nil, []string{"apartment id cannot be empty"})
+					return
+				}
+				err := s.DB.DeleteApartment(apartmentID, user.ID)
+				if err != nil {
+					log.Println(err.Error())
+					response.JSON(c, "", http.StatusInternalServerError, nil, []string{"internal server error"})
+					return
+				}
+				response.JSON(c, "Deleted Successfully", http.StatusOK, nil, nil)
+				return
+		}
+		}
+		log.Printf("can't get user from context\n")
+		response.JSON(c, "", http.StatusInternalServerError, nil, []string{"internal server error"})
 	}
 }

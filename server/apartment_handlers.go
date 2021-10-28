0d6edba42a502ed5885af47b0ea4a0ea6c24e227
nade-harlow <omonadefranklyn@gmail.com>
2021-10-28 17:12:18 +0100

    update apartment handler

--- server/apartment_handlers.go
+++ server/apartment_handlers.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"github.com/decadevs/rentals-api/models"
 	"github.com/decadevs/rentals-api/server/response"
 	"github.com/gin-gonic/gin"
 	"log"
@@ -9,9 +10,18 @@ import (
 
 func (s *Server) handleUpdateApartmentDetails() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Get("apartment")
+		apartment := &models.Apartment{}
+		if errs := s.decode(c, apartment); errs != nil {
+			response.JSON(c, "", http.StatusBadRequest, nil, errs)
+			return
+		}
 
-		log.Printf("can't get apartment from context\n")
-		response.JSON(c, "", http.StatusInternalServerError, nil, []string{"internal server error"})
+		if err := s.DB.UpdateApartment(apartment); err != nil {
+			log.Printf("update apartment error : %v\n", err)
+			response.JSON(c, "", http.StatusInternalServerError, nil, []string{"internal server error"})
+			return
+		}
+		response.JSON(c, "apartment updated successfully", http.StatusOK, nil, nil)
+		return
 	}
 }

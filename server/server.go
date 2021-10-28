13ceaf44e165ae86f690201bdc740123338077b9
nade-harlow <omonadefranklyn@gmail.com>
2021-10-28 17:18:46 +0100

    new route

--- server/server.go
+++ server/server.go
@@ -34,6 +34,7 @@ func (s *Server) defineRoutes(router *gin.Engine) {
 	authorized.GET("/users", s.handleGetUsers())
 	authorized.PUT("/me/update", s.handleUpdateUserDetails())
 	authorized.GET("/me", s.handleShowProfile())
+	apirouter.PUT("/user/apartment/update", s.handleUpdateApartmentDetails())
 }
 
 func (s *Server) setupRouter() *gin.Engine {

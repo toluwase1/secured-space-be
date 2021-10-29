8d1f05b9d8f11e2e8ba4eaca76952ea1aab3eba6
nade-harlow <omonadefranklyn@gmail.com>
2021-10-29 02:45:30 +0100

    added apartment id

--- server/apartment_handlers.go
+++ server/apartment_handlers.go
@@ -10,13 +10,18 @@ import (
 
 func (s *Server) handleUpdateApartmentDetails() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		apartmentID := c.Param("apartmentID")
+		if apartmentID == "" {
+			response.JSON(c, "", http.StatusBadRequest, nil, []string{"apartment id cannot be empty"})
+			return
+		}
 		apartment := &models.Apartment{}
 		if errs := s.decode(c, apartment); errs != nil {
 			response.JSON(c, "", http.StatusBadRequest, nil, errs)
 			return
 		}
 
-		if err := s.DB.UpdateApartment(apartment); err != nil {
+		if err := s.DB.UpdateApartment(apartment, apartmentID); err != nil {
 			log.Printf("update apartment error : %v\n", err)
 			response.JSON(c, "", http.StatusInternalServerError, nil, []string{"internal server error"})
 			return

8d86f12a1c81ec9f850242af3478b23ed61a8f72
Decagon <iroegbucalistus@gmail.com>
2021-10-29 03:25:00 +0100

    modified the create apartment handler

--- server/apartment_handler.go
+++ server/apartment_handler.go
@@ -1,12 +1,21 @@
 package server
 
 import (
+	"github.com/decadevs/rentals-api/models"
+	"github.com/decadevs/rentals-api/server/response"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func (s *Server) handleCreateApartment() gin.HandlerFunc {
+	// function to handle adding an apartment
 	return func(c *gin.Context) {
+		apartment := models.Apartment{}
 
+		if errs := s.decode(c, apartment); errs != nil {
+			response.JSON(c, "", http.StatusBadRequest, nil, errs)
+			return
+		}
 
 	}
 }

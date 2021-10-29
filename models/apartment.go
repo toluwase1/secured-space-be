9a2fb04235b604c64b88d221424e1434c315d5c1
Decagon <iroegbucalistus@gmail.com>
2021-10-29 02:05:09 +0100

    modified the apartment struct, added the create apartment handler

--- models/apartment.go
+++ models/apartment.go
@@ -8,7 +8,7 @@ const (
 )
 
 type Apartment struct {
-	ID              string          `gorm:"type:uuid; default:uuid_generate_v4();size:100; not null"`
+	ID              string          `sql:"type:uuid; default:uuid_generate_v4();size:100; not null"`
 	UserID          string          `json:"user_id" gorm:"foreignkey:User(id)"`
 	Title           string          `json:"title" gorm:"not null"`
 	CategoryID      string          `json:"category_id"  gorm:"foreignkey: categories(id)"`
@@ -17,8 +17,8 @@ type Apartment struct {
 	NoOfRooms       int             `json:"no_of_rooms"  gorm:"not null"`
 	Furnished       bool            `json:"furnished"  gorm:"not null"`
 	Location        string          `json:"location" gorm:"not null"`
-	ApartmentStatus ApartmentStatus `json:"apartment_status" gorm:"not null"`
-	//Images        	[]Image    `json:"images" gorm:"not null"`
+	ApartmentStatus ApartmentStatus `json:"apartment_status" gorm:"not null default:true"`
+	Images          []Images        `json:"images" gorm:"not null"`
 	//Interiors		[]Interior  `json:"interior" gorm:"not null; many2many:apartment_interior"`
 	//Exteriors     []Exterior   `json: "exterior" gorm: "not null; many2many:apartment_exterior"`
 }
---
+++ server/apartment_handler.go
@@ -0,0 +1,12 @@
+package server
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func (s *Server) handleCreateApartment() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+
+	}
+}

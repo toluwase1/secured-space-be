38f3aa3b9b8821c6dc46887ed28d7b99903961cb
Decagon <iroegbucalistus@gmail.com>
2021-10-29 02:21:03 +0100

    added the route for the create apartment handler

--- server/server.go
+++ server/server.go
@@ -34,6 +34,9 @@ func (s *Server) defineRoutes(router *gin.Engine) {
 	authorized.GET("/users", s.handleGetUsers())
 	authorized.PUT("/me/update", s.handleUpdateUserDetails())
 	authorized.GET("/me", s.handleShowProfile())
+
+	// apartment routes
+	authorized.POST("/apartments",s.handleCreateApartment())
 }
 
 func (s *Server) setupRouter() *gin.Engine {

6463e85229cb5e403d08a03d2cf8b2356c8f1579
nade-harlow <omonadefranklyn@gmail.com>
2021-10-31 18:46:43 +0100

    updated test

--- server/apartment_test.go
+++ server/apartment_test.go
@@ -6,11 +6,14 @@ import (
 	"github.com/decadevs/rentals-api/db"
 	"github.com/decadevs/rentals-api/models"
 	"github.com/decadevs/rentals-api/router"
+	"github.com/decadevs/rentals-api/services"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/golang/mock/gomock"
 	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"strings"
 	"testing"
 )
@@ -24,6 +27,11 @@ func TestUpdateApartment(t *testing.T) {
 		DB:     m,
 		Router: router.NewRouter(),
 	}
+	user := &models.User{}
+	secret := os.Getenv("JWT_SECRET")
+	accessClaims, _ := services.GenerateClaims("frank@gmail.com")
+	accToken, _ := services.GenerateToken(jwt.SigningMethodHS256, accessClaims, &secret)
+
 	route := s.setupRouter()
 	apartment := &models.Apartment{
 		Title:           "2 bedrooms",
@@ -37,15 +45,36 @@ func TestUpdateApartment(t *testing.T) {
 		Exteriors:       []models.ExteriorFeature{{Name: "Garage"}, {Name: "pool"}},
 	}
 
+	m.EXPECT().TokenInBlacklist(gomock.Any()).Return(false).Times(3)
+	m.EXPECT().FindUserByEmail("frank@gmail.com").Return(user, nil).Times(3)
+	m.EXPECT().UpdateApartment(apartment, apartmentID).Return(errors.New("error exist"))
 	m.EXPECT().UpdateApartment(apartment, apartmentID).Return(nil)
+
 	t.Run("testing empty apartment id", func(t *testing.T) {
 
 		w := httptest.NewRecorder()
 		req, _ := http.NewRequest("PUT", fmt.Sprintf("/api/v1/user/%s/update", ""), nil)
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", *accToken))
 		route.ServeHTTP(w, req)
 
 		assert.Equal(t, http.StatusBadRequest, w.Code)
 		assert.Contains(t, w.Body.String(), "apartment id cannot be empty")
+	})
+
+	t.Run("testing error", func(t *testing.T) {
+
+		jsonapartment, err := json.Marshal(apartment)
+		if err != nil {
+			t.Fail()
+			return
+		}
+		w := httptest.NewRecorder()
+		req, _ := http.NewRequest("PUT", fmt.Sprintf("/api/v1/user/%s/update", apartmentID), strings.NewReader(string(jsonapartment)))
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", *accToken))
+		route.ServeHTTP(w, req)
+
+		assert.Equal(t, http.StatusInternalServerError, w.Code)
+		assert.Contains(t, w.Body.String(), "internal server error")
 	})
 
 	t.Run("testing if error does not exist", func(t *testing.T) {
@@ -56,25 +85,10 @@ func TestUpdateApartment(t *testing.T) {
 		}
 		w := httptest.NewRecorder()
 		req, _ := http.NewRequest("PUT", fmt.Sprintf("/api/v1/user/%s/update", apartmentID), strings.NewReader(string(jsonapartment)))
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", *accToken))
 		route.ServeHTTP(w, req)
 
 		assert.Equal(t, http.StatusOK, w.Code)
 		assert.Contains(t, w.Body.String(), "apartment updated successfully")
 	})
-
-	m.EXPECT().UpdateApartment(apartment, apartmentID).Return(errors.New("error exist"))
-	t.Run("testing error", func(t *testing.T) {
-		jsonapartment, err := json.Marshal(apartment)
-		if err != nil {
-			t.Fail()
-			return
-		}
-		w := httptest.NewRecorder()
-		req, _ := http.NewRequest("PUT", fmt.Sprintf("/api/v1/user/%s/update", apartmentID), strings.NewReader(string(jsonapartment)))
-		route.ServeHTTP(w, req)
-
-		assert.Equal(t, http.StatusInternalServerError, w.Code)
-		assert.Contains(t, w.Body.String(), "internal server error")
-	})
-
 }

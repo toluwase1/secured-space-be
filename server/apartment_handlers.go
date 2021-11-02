b5c3a27b16fd12978d29acfc1255a31976b75dca
Ad3bay0c <aclintonadebayo@gmail.com>
2021-11-02 12:45:56 +0100

    Resolved Conflicts

--- server/apartment_handlers.go
+++ server/apartment_handlers.go
@@ -29,27 +29,27 @@ func (s *Server) DeleteApartment() gin.HandlerFunc {
 		}
 		log.Printf("can't get user from context\n")
 		response.JSON(c, "", http.StatusInternalServerError, nil, []string{"internal server error"})
-  }
-  
-func (s *Server) handleUpdateApartmentDetails() gin.HandlerFunc {
-	return func(c *gin.Context) {
+	}
+}
+	func(s *Server) handleUpdateApartmentDetails() gin.HandlerFunc{
+		return func (c *gin.Context){
 		apartmentID := c.Param("apartmentID")
-		if apartmentID == "" {
-			response.JSON(c, "", http.StatusBadRequest, nil, []string{"apartment id cannot be empty"})
-			return
-		}
+		if apartmentID == ""{
+		response.JSON(c, "", http.StatusBadRequest, nil, []string{"apartment id cannot be empty"})
+		return
+	}
 		apartment := &models.Apartment{}
-		if errs := s.decode(c, apartment); errs != nil {
-			response.JSON(c, "", http.StatusBadRequest, nil, errs)
-			return
-		}
+		if errs := s.decode(c, apartment); errs != nil{
+		response.JSON(c, "", http.StatusBadRequest, nil, errs)
+		return
+	}
 
-		if err := s.DB.UpdateApartment(apartment, apartmentID); err != nil {
-			log.Printf("update apartment error : %v\n", err)
-			response.JSON(c, "", http.StatusInternalServerError, nil, []string{"internal server error"})
-			return
-		}
+		if err := s.DB.UpdateApartment(apartment, apartmentID); err != nil{
+		log.Printf("update apartment error : %v\n", err)
+		response.JSON(c, "", http.StatusInternalServerError, nil, []string{"internal server error"})
+		return
+	}
 		response.JSON(c, "apartment updated successfully", http.StatusOK, nil, nil)
 		return
 	}
-}
+	}
--- server/apartment_test.go
+++ server/apartment_test.go
@@ -16,6 +16,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"strings"
 	"testing"
 )
 

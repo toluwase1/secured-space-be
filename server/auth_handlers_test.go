c5a8022b3d17820a6cc255650a8e4d937cc654dc
udodinho <udodinho200@gmail.com>
2021-11-04 22:24:22 +0100

    Testing working correctly

--- server/auth_handlers_test.go
+++ server/auth_handlers_test.go
@@ -66,7 +66,7 @@ func Test_handleLogin(t *testing.T) {
 
 		router.ServeHTTP(w, req)
 		assert.Equal(t, http.StatusOK, w.Code)
-		assert.Contains(t, w.Body.String(), "")
+		assert.Contains(t, w.Body.String(), loginRequest.Email)
 	})
 }
 

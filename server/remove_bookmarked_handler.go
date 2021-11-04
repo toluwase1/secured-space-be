330a43d9d36dfebaa4e2c855f9422cfca776ff0c
OShuaib <olayinka.shuaib@yahoo.com>
2021-11-04 23:29:11 +0100

    Refactoring of logic

--- server/remove_bookmarked_handler.go
+++ server/remove_bookmarked_handler.go
@@ -11,28 +11,31 @@ import (
 
 func (s *Server) RemoveBookmarkedApartment() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		if user, exists := c.Get("user"); exists {
-			if user, ok := user.(*models.User); ok {
-				apartmentID := strings.TrimSpace(c.Param("apartmentID"))
+		user, err := GetUserFromContext(c)
 
-				if ok := s.DB.CheckApartmentInBookmarkApartment(user.ID, apartmentID); !ok {
-					response.JSON(c, "", http.StatusBadRequest, nil, []string{"apartment not bookmarked"})
-					return
-				}
-				rmbookmarkApartment := &models.BookmarkApartment{
-					UserID:      user.ID,
-					ApartmentID: apartmentID,
-				}
-				if err := s.DB.RemoveBookmarkedApartment(rmbookmarkApartment); err != nil {
-					log.Printf(err.Error())
-					response.JSON(c, "", http.StatusInternalServerError, nil, []string{"internal server error"})
-					return
-				}
-				response.JSON(c, "Bookmarked Remove  Successfully", http.StatusOK, nil, nil)
-				return
-			}
+		if err != nil {
+			response.JSON(c, "", http.StatusUnauthorized, nil, []string{"user not authorized"})
 		}
-		log.Printf("can't get user from context\n")
-		response.JSON(c, "", http.StatusInternalServerError, nil, []string{"internal server error"})
+		apartmentID := strings.TrimSpace(c.Param("apartmentID"))
+
+		if ok := s.DB.CheckApartmentInBookmarkApartment(user.ID, apartmentID); !ok {
+			response.JSON(c, "", http.StatusBadRequest, nil, []string{"apartment not bookmarked"})
+			return
+		}
+
+		rmbookmarkApartment := &models.BookmarkApartment{
+			UserID:      user.ID,
+			ApartmentID: apartmentID,
+		}
+
+		if err := s.DB.RemoveBookmarkedApartment(rmbookmarkApartment); err != nil {
+			log.Printf(err.Error())
+			response.JSON(c, "", http.StatusInternalServerError, nil, []string{"internal server error"})
+			return
+		}
+		response.JSON(c, "Bookmarked Remove  Successfully", http.StatusOK, nil, nil)
+		return
 	}
+
 }
+

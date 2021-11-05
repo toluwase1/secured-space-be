bd65ece9eacb6a64e3f3a0c739390fdc2bec4ba5
techagentng <techagentng@gmail.com>
2021-11-05 06:41:13 +0100

    abstracted GetUserFromContext method from GetBookmarkedApartments method

--- server/user_bookedmarkedApartments.go
+++ server/user_bookedmarkedApartments.go
@@ -1,24 +1,24 @@
 package server
 
 import (
-	"github.com/decadevs/rentals-api/models"
 	"github.com/decadevs/rentals-api/server/response"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
-// GetBookmarkedApartments lists all bookmarked apartments
 func (s *Server) GetBookmarkedApartments() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		if userI, exists := c.Get("user"); exists {
-			if user, ok := userI.(*models.User); ok {
-				bookmarkedApartment, err := s.DB.GetBookmarkedApartments(user.ID)
-				if err != nil {
-					response.JSON(c, "", http.StatusInternalServerError, nil, []string{"internal server error"})
-					return
-				}
-				response.JSON(c, "retrieved bookmarks successfully", http.StatusOK, bookmarkedApartment, nil)
-			}
+		user, err := GetUserFromContext(c)
+		if err != nil {
+			response.JSON(c, "", http.StatusUnauthorized, nil, []string{"user not authorized"})
+			return
 		}
+		bookmarkedApartment, err := s.DB.GetBookmarkedApartments(user.ID)
+		if err != nil {
+			response.JSON(c, "", http.StatusInternalServerError, nil, []string{"internal server error"})
+			return
+		}
+		response.JSON(c, "retrieved bookmarks successfully", http.StatusOK, bookmarkedApartment, nil)
+		return
 	}
 }

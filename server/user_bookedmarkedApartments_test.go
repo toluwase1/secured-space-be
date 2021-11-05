a478a58294cb15c60fd55b900899838c2b650f4d
techagentng <techagentng@gmail.com>
2021-11-05 09:37:55 +0100

    fixed conflicts

--- server/user_bookedmarkedApartments_test.go
+++ server/user_bookedmarkedApartments_test.go
@@ -28,10 +28,6 @@ func TestApplication_BookmarkedApartments(t *testing.T) {
 	user := &models.User{
 		Email: "adebayo@gmail.com",
 	}
-	//bookmarks := &models.BookmarkApartment{
-	//	UserID: "techagentng@gmail.com",
-	//	ApartmentID: "q2342342ccwefcwef",
-	//}
 	user.ID = "cad3ac6e-ad3d-46c4-8b20-4283525c6136"
 	secret := os.Getenv("JWT_SECRET")
 	accessClaims, refreshClaims := services.GenerateClaims(user.Email)
@@ -40,7 +36,6 @@ func TestApplication_BookmarkedApartments(t *testing.T) {
 
 	mockedDB.EXPECT().TokenInBlacklist(gomock.Any()).Return(false).Times(2)
 	mockedDB.EXPECT().FindUserByEmail(user.Email).Return(user, nil).Times(2)
-	//mockedDB.EXPECT().GetBookmarkedApartment(user.ID).Return(user.ID, nil).Times(2)
 
 	mockedDB.EXPECT().GetBookmarkedApartments(user.ID).Return(nil, nil)
 	t.Run("Test_For_Successful_Response", func(t *testing.T) {

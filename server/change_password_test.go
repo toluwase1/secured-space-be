7fea48c82e41c4bb902df74a2332f75097ecdf8a
Ad3bay0c <aclintonadebayo@gmail.com>
2021-11-13 20:46:37 +0100

    Comprehensive test for change password

--- server/change_password_test.go
+++ server/change_password_test.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/decadevs/rentals-api/db"
 	"github.com/decadevs/rentals-api/router"
 	"github.com/golang/mock/gomock"
@@ -13,7 +14,7 @@ import (
 	"testing"
 )
 
-func TestApplication_ResetPassword(t *testing.T) {
+func TestApplication_ChangePassword(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	mockedDb := db.NewMockDB(ctrl)
 
@@ -22,37 +23,67 @@ func TestApplication_ResetPassword(t *testing.T) {
 		Router: router.NewRouter(),
 	}
 	r := s.setupRouter()
-
+	accToken := AuthorizeTestRoutes(mockedDb, t)
 	t.Run("Test_For_Missing_Password", func(t *testing.T) {
-		requestPassword := struct {
-			Password  string
+		passwordInfo := struct {
+			OldPassword	string `json:"old_password" binding:"required"`
+			NewPassword string `json:"new_password" binding:"required"`
 		}{
-			Password: "",
+			OldPassword: "",
+            NewPassword: "",
 		}
-		body, _ := json.Marshal(requestPassword)
-		req, err := http.NewRequest(http.MethodPost, "/api/v1/reset-password/12340987wertt", strings.NewReader(string(body)))
+		body, _ := json.Marshal(passwordInfo)
+		req, err := http.NewRequest(http.MethodPost, "/api/v1/user/change-password", strings.NewReader(string(body)))
 		if err != nil {
 			t.Errorf("Error Creating Request: %v", err.Error())
 		}
 		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", *accToken))
 		res := httptest.NewRecorder()
 
 		r.ServeHTTP(res, req)
-		assert.Contains(t, res.Body.String(), "validation failed on field 'Password'")
+		assert.Contains(t, res.Body.String(), "validation failed on field 'OldPassword', condition: required")
 		assert.Equal(t, http.StatusBadRequest, res.Code)
 	})
-	requestPassword := struct {
-		Password  string
-	}{
-		Password: "Password",
-	}
-	mockedDb.EXPECT().ResetPassword("12340987wertt", gomock.Any()).Return(errors.New("an error occurred"))
-	t.Run("Test_For_Reset_Password_Error", func(t *testing.T) {
-		body, _ := json.Marshal(requestPassword)
-		req, err := http.NewRequest(http.MethodPost, "/api/v1/reset-password/12340987wertt", strings.NewReader(string(body)))
+	accToken = AuthorizeTestRoutes(mockedDb, t)
+	t.Run("Test_For_Incorrect_Password", func(t *testing.T) {
+		passwordInfo := struct {
+			OldPassword	string `json:"old_password" binding:"required"`
+			NewPassword string `json:"new_password" binding:"required"`
+		}{
+			OldPassword: "password123",
+			NewPassword: "password",
+		}
+		body, _ := json.Marshal(passwordInfo)
+		req, err := http.NewRequest(http.MethodPost, "/api/v1/user/change-password", strings.NewReader(string(body)))
 		if err != nil {
 			t.Errorf("Error Creating Request: %v", err.Error())
 		}
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", *accToken))
+		req.Header.Set("Content-Type", "application/json")
+		res := httptest.NewRecorder()
+
+		r.ServeHTTP(res, req)
+		assert.Contains(t, res.Body.String(), "Incorrect Password Details")
+		assert.Equal(t, http.StatusBadRequest, res.Code)
+	})
+
+	accToken = AuthorizeTestRoutes(mockedDb, t)
+	mockedDb.EXPECT().ResetPassword("1234567asdf", gomock.Any()).Return(errors.New("an error occurred"))
+	t.Run("Test_For_Password_Changed_Error", func(t *testing.T) {
+		passwordInfo := struct {
+			OldPassword	string `json:"old_password" binding:"required"`
+			NewPassword string `json:"new_password" binding:"required"`
+		}{
+			OldPassword: "password",
+			NewPassword: "password123",
+		}
+		body, _ := json.Marshal(passwordInfo)
+		req, err := http.NewRequest(http.MethodPost, "/api/v1/user/change-password", strings.NewReader(string(body)))
+		if err != nil {
+			t.Errorf("Error Creating Request: %v", err.Error())
+		}
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", *accToken))
 		req.Header.Set("Content-Type", "application/json")
 		res := httptest.NewRecorder()
 
@@ -60,18 +91,27 @@ func TestApplication_ResetPassword(t *testing.T) {
 		assert.Contains(t, res.Body.String(), "Internal Server Error")
 		assert.Equal(t, http.StatusInternalServerError, res.Code)
 	})
-	mockedDb.EXPECT().ResetPassword("12340987wertt", gomock.Any()).Return(nil)
-	t.Run("Test_For_Reset_Password_Error", func(t *testing.T) {
-		body, _ := json.Marshal(requestPassword)
-		req, err := http.NewRequest(http.MethodPost, "/api/v1/reset-password/12340987wertt", strings.NewReader(string(body)))
+	accToken = AuthorizeTestRoutes(mockedDb, t)
+	mockedDb.EXPECT().ResetPassword("1234567asdf", gomock.Any()).Return(nil)
+	t.Run("Test_For_Successfully_Password_Change", func(t *testing.T) {
+		passwordInfo := struct {
+			OldPassword	string `json:"old_password" binding:"required"`
+			NewPassword string `json:"new_password" binding:"required"`
+		}{
+			OldPassword: "password",
+			NewPassword: "password123",
+		}
+		body, _ := json.Marshal(passwordInfo)
+		req, err := http.NewRequest(http.MethodPost, "/api/v1/user/change-password", strings.NewReader(string(body)))
 		if err != nil {
 			t.Errorf("Error Creating Request: %v", err.Error())
 		}
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", *accToken))
 		req.Header.Set("Content-Type", "application/json")
 		res := httptest.NewRecorder()
 
 		r.ServeHTTP(res, req)
-		assert.Contains(t, res.Body.String(), "Password Reset Successfully")
+		assert.Contains(t, res.Body.String(), "Password Changed Successfully")
 		assert.Equal(t, http.StatusOK, res.Code)
 	})
 }

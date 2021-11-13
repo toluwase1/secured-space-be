630fb47a4ed08e24574a5e1533d48952b6a428ad
Ad3bay0c <aclintonadebayo@gmail.com>
2021-11-13 15:25:17 +0100

    Get User from context added to ChangePassword handler

--- server/change_password.go
+++ server/change_password.go
@@ -1,1 +1,16 @@
 package server
+
+import (
+	"github.com/decadevs/rentals-api/server/response"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func (s *Server) ChangePassword() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		user, err := GetUserFromContext(c)
+		if err != nil {
+			response.JSON(c, "", http.StatusInternalServerError, nil, []string{"Internal Server Error"})
+		}
+	}
+}

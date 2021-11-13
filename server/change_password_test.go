f67a45fdbb120ab6d601e5f5c18736bef9aaa0e4
Ad3bay0c <aclintonadebayo@gmail.com>
2021-11-13 21:39:07 +0100

    HashedPassword field removed from the testing user struct

--- server/change_password_test.go
+++ server/change_password_test.go
@@ -4,16 +4,35 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/decadevs/rentals-api/db"
+	"github.com/decadevs/rentals-api/models"
 	"github.com/decadevs/rentals-api/router"
+	"github.com/decadevs/rentals-api/services"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/golang/mock/gomock"
 	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"strings"
 	"testing"
 )
+func CheckAuthorize(m *db.MockDB, t *testing.T) *string {
+	accessClaims, _ := services.GenerateClaims("adebayo@gmail.com")
 
+	secret := os.Getenv("JWT_SECRET")
+	accToken, err := services.GenerateToken(jwt.SigningMethodHS256, accessClaims, &secret)
+	if err != nil {
+		t.Fail()
+	}
+
+	// "password" was hashed to form the hashedPassword data, and it is for testing purpose
+	user := &models.User{Email: "adebayo@gmail.com", HashedPassword: "$2a$10$dP0JsmQr4ycXj8MQJVaDkedmOA2owa7lAOKUqOmwEN3IbDOfkLROy"}
+	user.ID = "1234567asdf"
+	m.EXPECT().FindUserByEmail(user.Email).Return(user, nil)
+	m.EXPECT().TokenInBlacklist(accToken).Return(false)
+	return accToken
+}
 func TestApplication_ChangePassword(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	mockedDb := db.NewMockDB(ctrl)
@@ -23,7 +42,7 @@ func TestApplication_ChangePassword(t *testing.T) {
 		Router: router.NewRouter(),
 	}
 	r := s.setupRouter()
-	accToken := AuthorizeTestRoutes(mockedDb, t)
+	accToken := CheckAuthorize(mockedDb, t)
 	t.Run("Test_For_Missing_Password", func(t *testing.T) {
 		passwordInfo := struct {
 			OldPassword	string `json:"old_password" binding:"required"`
@@ -45,7 +64,7 @@ func TestApplication_ChangePassword(t *testing.T) {
 		assert.Contains(t, res.Body.String(), "validation failed on field 'OldPassword', condition: required")
 		assert.Equal(t, http.StatusBadRequest, res.Code)
 	})
-	accToken = AuthorizeTestRoutes(mockedDb, t)
+	accToken = CheckAuthorize(mockedDb, t)
 	t.Run("Test_For_Incorrect_Password", func(t *testing.T) {
 		passwordInfo := struct {
 			OldPassword	string `json:"old_password" binding:"required"`
@@ -68,7 +87,7 @@ func TestApplication_ChangePassword(t *testing.T) {
 		assert.Equal(t, http.StatusBadRequest, res.Code)
 	})
 
-	accToken = AuthorizeTestRoutes(mockedDb, t)
+	accToken = CheckAuthorize(mockedDb, t)
 	mockedDb.EXPECT().ResetPassword("1234567asdf", gomock.Any()).Return(errors.New("an error occurred"))
 	t.Run("Test_For_Password_Changed_Error", func(t *testing.T) {
 		passwordInfo := struct {
@@ -91,7 +110,7 @@ func TestApplication_ChangePassword(t *testing.T) {
 		assert.Contains(t, res.Body.String(), "Internal Server Error")
 		assert.Equal(t, http.StatusInternalServerError, res.Code)
 	})
-	accToken = AuthorizeTestRoutes(mockedDb, t)
+	accToken = CheckAuthorize(mockedDb, t)
 	mockedDb.EXPECT().ResetPassword("1234567asdf", gomock.Any()).Return(nil)
 	t.Run("Test_For_Successfully_Password_Change", func(t *testing.T) {
 		passwordInfo := struct {

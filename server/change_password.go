c277b9782c6223e59354b22499c2c01fe61d8076
Ad3bay0c <aclintonadebayo@gmail.com>
2021-11-13 19:45:42 +0100

    Change Password handler logic added

--- server/change_password.go
+++ server/change_password.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"github.com/decadevs/rentals-api/server/response"
+	"github.com/decadevs/rentals-api/services"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -11,6 +12,31 @@ func (s *Server) ChangePassword() gin.HandlerFunc {
 		user, err := GetUserFromContext(c)
 		if err != nil {
 			response.JSON(c, "", http.StatusInternalServerError, nil, []string{"Internal Server Error"})
+			return
 		}
+		passwordInfo := struct {
+			OldPassword	string `json:"old_password" binding:"required"`
+			NewPassword string `json:"new_password" binding:"required"`
+		}{}
+		if errs := s.decode(c, &passwordInfo); errs != nil {
+			response.JSON(c, "", http.StatusInternalServerError, nil, errs)
+			return
+		}
+		err = services.CompareHashAndPassword([]byte(passwordInfo.OldPassword), user.HashedPassword)
+		if err != nil {
+			response.JSON(c, "", http.StatusBadRequest, nil, []string{"Incorrect Password"})
+			return
+		}
+		hashedPassword, err := services.GenerateHashPassword(passwordInfo.NewPassword)
+		if err != nil {
+			response.JSON(c, "", http.StatusInternalServerError, nil, []string{"Internal Server Error"})
+            return
+		}
+		err = s.DB.ResetPassword(user.ID, string(hashedPassword))
+		if err != nil {
+			response.JSON(c, "", http.StatusInternalServerError, nil, []string{"Internal Server Error"})
+            return
+		}
+		response.JSON(c, "Password Changed Successfully", http.StatusOK, nil, nil)
 	}
 }

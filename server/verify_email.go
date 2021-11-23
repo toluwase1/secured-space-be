22c8753564bc7985d02c3b428fae670991c3afa5
tambarie <gt.gbaragbo@gmail.com>
2021-11-23 23:28:10 +0100

    add verify email

--- server/verify_email.go
+++ server/verify_email.go
@@ -1,1 +1,16 @@
 package server
+//
+//import "github.com/gin-gonic/gin"
+//
+//func (s *Server) VerifyEmail() gin.HandlerFunc  {
+//	return func(c  *gin.Context) {
+//
+//	userID := c.Param("userID")
+//
+//
+//
+//
+//
+//
+//	}
+//}
